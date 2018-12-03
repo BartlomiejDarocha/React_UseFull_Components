3b9c2801ce67536f6dfb8ea9aaeb15087c90bc4b
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2018-12-03 16:06:12 +0100

    ...

--- js/Carusel.jsx
+++ js/Carusel.jsx
@@ -8,13 +8,38 @@ class Carusel extends React .Component{
         super(props);
         this.state={
             cCount:0,
+            Testprops:[1,2,3,4],
         }
     }
     minusCounter = () =>{
-        this.setState({cCount:this.state.cCount-1})
+        if(this.state.cCount <=0){
+            this.setState({cCount:this.state.Testprops.length-1},()=>{
+                console.log(this.state.cCount, 'minus po if');
+            })
+        }else{
+            this.setState({cCount:this.state.cCount-1},()=>{
+                console.log(this.state.cCount, 'minus po else');
+            })
+        }
     }
     plusCounter =()=>{
-        this.setState({cCount:this.state.cCount+1},()=>{
+        if(this.state.cCount>= this.state.Testprops.length-1){
+            this.setState({cCount:0},()=>{
+                console.log(this.state.cCount, 'plus po if');
+            })
+        }else{
+            this.setState({cCount:this.state.cCount+1},()=>{
+                console.log(this.state.cCount, 'plus po else');
+            })
+        }
+        
+    }
+    setOn=(e)=>{
+        console.log("test");
+        let tempVal = e.target.dataset.id;
+        this.setState({
+            cCount: tempVal,
+        },()=>{
             console.log(this.state.cCount);
         })
     }
@@ -23,11 +48,16 @@ class Carusel extends React .Component{
             <div>
                 <button onClick={this.minusCounter}>Left</button>
                 <button onClick ={this.plusCounter}>Right</button>
+                <button data-id ='0' onClick ={this.setOn}>0</button>
+                <button data-id ='1' onClick ={this.setOn}>1</button>
+                <button data-id ='2' onClick ={this.setOn}>2</button>
+                <button data-id ='3' onClick ={this.setOn}>3</button>
                 <div className="carusel_wrapper">
                     <ul className="listbox">
-                        <li className ="carusel_item" style={{'backgroundColor':'red'}}></li>
-                        <li className ="carusel_item" style={{'backgroundColor':'blue'}}></li>
-                        <li className ="carusel_item" style={{'backgroundColor':'green'}}></li>
+                        <li className ={`${this.state.cCount === 0? 'active':'' || this.state.cCount===1? 'disactive': ''} carusel_item`} style={{'backgroundColor':'red'}}></li>
+                        <li className ={`${this.state.cCount === 1? 'active':'' || this.state.cCount===2? 'disactive': ''} carusel_item`} style={{'backgroundColor':'blue'}}></li>
+                        <li className ={`${this.state.cCount === 2? 'active':'' || this.state.cCount===3? 'disactive': ''} carusel_item`} style={{'backgroundColor':'green'}}></li>
+                        <li className ={`${this.state.cCount === 3? 'active':'' || this.state.cCount===0? 'disactive': ''} carusel_item`} style={{'backgroundColor':'orange'}}></li>
                     </ul>   
                 </div>
             </div>
