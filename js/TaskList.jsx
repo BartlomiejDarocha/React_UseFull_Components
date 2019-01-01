f0fe498e4af23cfa84b1a90e055a488e1cbde9ae
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-01-01 23:22:34 +0100

    tasklist

--- js/TaskList.jsx
+++ js/TaskList.jsx
@@ -6,17 +6,19 @@ class TaskList extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            inputText: 'it will be change',
+            inputText: '',
+            h1Text: 'it will be change',
             list: [],
         }
     }
     lengthInputTest =()=>{
         if(this.state.inputText.length <= 0){
-            this.setState({inputText:'it will be change',})
+            this.setState({h1Text:'it will be change',})
         }
     }
     handleText =(e)=> {
         this.setState({
+            h1Text:'',
             inputText: e.target.value,
         });
     }
@@ -24,7 +26,7 @@ class TaskList extends React.Component{
         event.preventDefault();
         let tempArry = this.state.list.slice();
         tempArry.push(this.state.inputText);
-        this.setState({inputText:'', list: tempArry})
+        this.setState({list: tempArry, inputText:''})
     }
     componentDidUpdate(){
         this.lengthInputTest();   
@@ -36,9 +38,9 @@ class TaskList extends React.Component{
         })
         return (
             <div>
-                <h1>{this.state.inputText}</h1>
+                <h1>{this.state.h1Text}{this.state.inputText}</h1>
                 <form>
-                    <input className="inPutText" type="text" onChange = {this.handleText} size="20" placeholder="New Task"/>
+                    <input className="inPutText" type="text" onChange = {this.handleText} value={this.state.inputText} size="20" placeholder="New Task"/>
                     <button onClick={this.handleAddButton}>Dodaj</button>
                 </form>
                 <ul>
